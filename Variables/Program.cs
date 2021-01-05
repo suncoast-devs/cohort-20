90abb33872a10e221cedfec01ca09787ea0e8f69
Gavin Stark <gavin@gstark.com>
2021-01-05 11:21:17 -0500

    Implemented other kinds of variables

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -13,6 +13,21 @@ namespace Variables
 
             // This creates a number
             var score = 95;
+
+            // Work with booleans
+            var theCakeIsALie = true;
+            var worldIsFlat = false;
+            var notrequired = true;
+            var justConvention = true;
+
+            // Make a new string and learn how to get the length
+            var sentence = "The quick brown fox jumped over the lazy dog";
+            var howLong = sentence.Length;
+
+            // Work with characters from a string
+            var firstLetter = sentence[0];
+            var secondLetter = sentence[1];
+            var thirdLetter = sentence[2];
         }
     }
 }
