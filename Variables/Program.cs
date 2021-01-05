b6eb97b92a31aae76936c76e6a82f067404c9e66
Gavin Stark <gavin@gstark.com>
2021-01-05 11:10:58 -0500

    Simple variable declaration

---
+++ Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+
+            // This is a string
+            var name = "Samantha";
+
+            // This creates a number
+            var score = 95;
+        }
+    }
+}
