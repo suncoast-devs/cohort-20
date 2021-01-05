0571c1917c92848b79a9ac9e75a612a7428b37bf
Gavin Stark <gavin@gstark.com>
2021-01-05 08:47:57 -0500

    Hello World

---
+++ HelloDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, my name is Gavin.");
+        }
+    }
+}
