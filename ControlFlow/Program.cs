471cf8d3f841f0e5841962e81005a2d9c274c578
Gavin Stark <gavin@gstark.com>
2021-01-06 11:00:01 -0500

    if and switch statemenst

---
+++ ControlFlow/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ControlFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Sam";
+
+            if (name == "Paul")
+            {
+                Console.WriteLine("Here");
+            }
+            else if (name == "Dorothy")
+            {
+                Console.WriteLine("Also here");
+            }
+            else if (name == "Sam")
+            {
+                Console.WriteLine("Here again");
+            }
+            else
+            {
+                Console.WriteLine("Didn't find anything");
+            }
+
+            Console.WriteLine("Pick back up here...");
+
+
+            switch (name)
+            {
+                case "Paul":
+                    Console.WriteLine("Here");
+                    break;
+                case "Dorothy":
+                    Console.WriteLine("Also Here");
+                    break;
+                case "Sam":
+                    Console.WriteLine("Here Again");
+                    break;
+                default:
+                    Console.WriteLine("Didn't find anything");
+                    break;
+            }
+
+            var score = 100;
+
+            switch (score)
+            {
+                case < 20:
+                    Console.WriteLine("The score is good");
+                    break;
+
+                case > 95:
+                    Console.WriteLine("This score is really good");
+                    break;
+            }
+
+            Console.WriteLine("Pick back up here after the switch...");
+        }
+    }
+}
