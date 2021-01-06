05008ec1b3c356fc691adf97ca7ca0b3b63dbf9e
Gavin Stark <gavin@gstark.com>
2021-01-06 10:45:55 -0500

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("What is your name? ");
+            // var name = Console.ReadLine();
+
+            // while (name != "quit")
+            // {
+            //     Console.WriteLine($"Hello {name}");
+
+            //     Console.Write("What is your name? ");
+            //     name = Console.ReadLine();
+            // }
+
+            var currentNumber = 0;
+
+            while (currentNumber < 10)
+            {
+                Console.WriteLine(currentNumber);
+                currentNumber = currentNumber + 1;
+            }
+
+            Console.WriteLine($"The number is now {currentNumber}");
+        }
+    }
+}
