0f20e2707d1cbb78b034052c3b1bb89df0a40f2c
Gavin Stark <gavin@gstark.com>
2021-01-07 11:25:02 -0500

    Created two methods, DisplayGreeting and PromptForString

---
+++ Methods/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        // New Methods Go Here!!!!
+
+        // static method (ignore this for the moment)
+        //  |
+        //  |  The return (output) type. Here there is none
+        //  |  since the method isn't giving anything back
+        //  |  to the code that called it.
+        //  |   |
+        //  |   |               The inputs, known as arguments. None in this case.
+        //  |   |                   |
+        //  |   |                   |
+        //  v   v                   v
+        static void DisplayGreeting()
+        {
+            //
+            //    Body of the method
+            //      |
+            //      |
+            //      v
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Welcome to Our Employee Database");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayGreeting();
+
+            // Console.Write("What is your name? ");
+            // var name = Console.ReadLine();
+            var name = PromptForString("What is your name? ");
+
+            // Console.Write("What is your favorite color? ");
+            // var favoriteColor = Console.ReadLine();
+            var favoriteColor = PromptForString("What is your favorite color? ");
+
+            Console.Write("What is your department number? ");
+            var department = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your yearly salary (in dollars)? ");
+            var salary = int.Parse(Console.ReadLine());
+
+            var salaryPerMonth = salary / 12;
+            Console.WriteLine($"Hello, {name} you make {salaryPerMonth} a month.");
+        }
+    }
+}
