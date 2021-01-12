5e8fc3cf2e0f231cbf1b18bb036184cc6056dec6
Gavin Stark <gavin@gstark.com>
2021-01-12 09:58:54 -0500

    Initial commit for LINQ

---
+++ LINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scores = new List<int> { 42, 100, 98, 15 };
+        }
+    }
+}
