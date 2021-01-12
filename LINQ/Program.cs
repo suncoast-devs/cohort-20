d51b040b13932967dcfb6b48865c37ead61e8ed0
Gavin Stark <gavin@gstark.com>
2021-01-12 10:10:16 -0500

    Doubling numbers using a function expression

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -7,7 +7,27 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            var scores = new List<int> { 42, 100, 98, 15 };
+            // Here is our original array
+            var scores = new List<int> { 42, 100, 98, 15, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            // Here is our handy Double-er
+            Func<int, int> MultiplyBy2 = value => value * 2;
+
+            // Make a new list to store the results
+            var newScores = new List<int>();
+
+            // Go through each score in the scores list
+            foreach (var score in scores)
+            {
+                // Make a doubling of that score
+                var doubled = MultiplyBy2(score);
+
+                // Add it to our new list
+                newScores.Add(doubled);
+            }
+
+            // Print out the scores comma separated
+            Console.WriteLine(String.Join(',', newScores));
         }
     }
 }
