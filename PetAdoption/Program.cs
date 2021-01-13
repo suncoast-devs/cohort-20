3849be344fa7bc6757f8f43676615201c128b46f
Gavin Stark <gavin@gstark.com>
2021-01-13 10:49:06 -0500

    Now can handle exiting our menu system

--- PetAdoption/Program.cs
+++ PetAdoption/Program.cs
@@ -22,17 +22,34 @@ namespace PetAdoption
             BannerMessage("Welcome to our Pet Adoption Agency");
 
             // While the user hasn't chosen to quit the application
-            //   Show them a menu of options they can do:
-            Console.WriteLine();
-            Console.WriteLine("Menu:");
-            Console.WriteLine("ADD - Add a new pet");
-            Console.WriteLine("SEE - See all the pets");
-            Console.WriteLine("ADOPT - Adopt a pet");
-            Console.WriteLine("UPDATE - Update a pet");
-            Console.WriteLine("QUIT - Quit the app");
-            Console.WriteLine();
-            Console.Write("What is your choice? ");
-            var choice = Console.ReadLine();
+            // Problem: want to keep a loop going until the user choose to quit
+            // Data: A boolean value that indicates if they have opted to quit
+            // Algorithm:
+            //       Before the loop, set our boolean to false: they haven't quit
+            var userHasChosenToQuit = false;
+            //       while our boolean is false
+            while (userHasChosenToQuit == false)
+            {
+                //          show the menu
+                //   Show them a menu of options they can do:
+                Console.WriteLine();
+                Console.WriteLine("Menu:");
+                Console.WriteLine("ADD - Add a new pet");
+                Console.WriteLine("SEE - See all the pets");
+                Console.WriteLine("ADOPT - Adopt a pet");
+                Console.WriteLine("UPDATE - Update a pet");
+                Console.WriteLine("QUIT - Quit the app");
+                Console.WriteLine();
+                Console.Write("What is your choice? ");
+                var choice = Console.ReadLine().ToUpper().Trim();
+
+
+                //          if the user's choice is "QUIT", set the boolean to true
+                if (choice == "QUIT")
+                {
+                    userHasChosenToQuit = true;
+                }
+            }
 
             //     - Add a new pet
             //     - See all the pets up for adoption
