3cec2f3b7ed94770ab3d0b28a4d7c18a7483d81e
Gavin Stark <gavin@gstark.com>
2021-01-14 09:44:34 -0500

    Initial program to keep a list of numbers

--- NumberTracker/Program.cs
+++ NumberTracker/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NumberTracker
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            bool hasQuit = false;
+            while (hasQuit == false)
+            {
+                Console.WriteLine();
+                // Print out our numbers on one line seperated by spaces
+                Console.WriteLine(String.Join(" ", numbers));
+
+                Console.Write("What is a number you'd like to track? (blank to quit) ");
+                var numberString = Console.ReadLine().Trim();
+                if (numberString == "")
+                {
+                    hasQuit = true;
+                }
+                else
+                {
+                    var number = int.Parse(numberString);
+
+                    numbers.Add(number);
+                }
+            }
+        }
     }
-  }
 }
