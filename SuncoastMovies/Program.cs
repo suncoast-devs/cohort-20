0e58dd60f9834799da0183c2b0ea195bc9f14c3c
Gavin Stark <gavin@gstark.com>
2021-01-21 10:24:42 -0500

    Uses the database context

--- SuncoastMovies/Program.cs
+++ SuncoastMovies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace SuncoastMovies
 {
@@ -7,7 +8,16 @@ namespace SuncoastMovies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C#");
+            // Get a new context which will connect to the database
+            var context = new SuncoastMoviesContext();
+
+            // Get a reference to our collection of movies.
+            // NOTE: this doesn't yet access any of them, just gives
+            //       us a variable that knows how.
+            var movies = context.Movies;
+
+            var movieCount = movies.Count();
+            Console.WriteLine($"There are {movieCount} movies!");
         }
     }
 }
--- SuncoastMovies/SuncoastMoviesContext.cs
+++ SuncoastMovies/SuncoastMoviesContext.cs
@@ -4,10 +4,27 @@ namespace SuncoastMovies
 {
     public class SuncoastMoviesContext : DbContext
     {
+        //                  The movies table....
+        //
+        //     Make available as a property named "Movies"
+        //
+        //                     Table name
+        //                     |
+        //           Class name|
+        //           |         |
+        //     Kinda | Like a L|ist<Movie>
+        //     |     |         |
         public DbSet<Movie> Movies { get; set; }
+        //
+        // Eventually this will be a long list of DbSet<things> where we have one
+        // property (DbSet) for every table we need to work with
+        //
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //                                                  This is what you would type >    pgcli SuncoastMovies
+            //                                                  |
+            //                                                  |
             optionsBuilder.UseNpgsql("server=localhost;database=SuncoastMovies");
         }
     }
