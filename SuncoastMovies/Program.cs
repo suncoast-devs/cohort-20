e734f24c8326fb7dc9d8936cda99c2b59834eb75
Gavin Stark <gavin@gstark.com>
2021-01-21 10:50:53 -0500

    Using `foreach` to loop through the movies in the table

--- SuncoastMovies/Program.cs
+++ SuncoastMovies/Program.cs
@@ -14,10 +14,15 @@ namespace SuncoastMovies
             // Get a reference to our collection of movies.
             // NOTE: this doesn't yet access any of them, just gives
             //       us a variable that knows how.
-            var movies = context.Movies;
+            // var movies = context.Movies;
 
-            var movieCount = movies.Count();
+            var movieCount = context.Movies.Count();
             Console.WriteLine($"There are {movieCount} movies!");
+
+            foreach (var movie in context.Movies)
+            {
+                Console.WriteLine($"There is a movie named {movie.Title}");
+            }
         }
     }
 }
