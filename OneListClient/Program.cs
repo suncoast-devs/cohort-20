f5aa1e139fa919c2f0e92bcdb349614da156df66
Gavin Stark <gavin@gstark.com>
2021-01-26 10:27:07 -0500

    Use the JSON deserializer to turn the big blob of JSON into a nice List<Item> -- print out how many items

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -1,17 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace OneListClient
 {
+    class Item
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public bool complete { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+    }
+
+
+
     class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static async Task Main(string[] args)
         {
             var client = new HttpClient();
 
-            var responseAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=illustriousvoyage");
+            // Make a `GET` request to the API and get back a *stream* of data.
+            var responseAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token=illustriousvoyage");
 
-            Console.WriteLine(responseAsString);
+            // Supply that *stream of data* to a Deserialize that will interpret it as a List of Item objects.
+            //
+            //                                                                   stream of data from api
+            //
+            //                                                       We want to get it back as a List of Item objects
+            //
+            //                                      Deserialize (turn a stream into a List)
+            //
+            //                       The Data is JSON
+            List<Item> items = await JsonSerializer.DeserializeAsync<List<Item>>(responseAsStream);
+
+            Console.WriteLine($"There are {items.Count()} todo items");
         }
     }
 }
