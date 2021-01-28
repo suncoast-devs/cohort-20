25bcef32ad1d25ec67122ff899d95df33042b611
Gavin Stark <gavin@gstark.com>
2021-01-28 11:50:32 -0500

    Migraton for adding Players

--- GameNight/Migrations/20210128164835_AddPlayers.Designer.cs
+++ GameNight/Migrations/20210128164835_AddPlayers.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using GameNight.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace GameNight.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210128164835_AddPlayers")]
+    partial class AddPlayers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
