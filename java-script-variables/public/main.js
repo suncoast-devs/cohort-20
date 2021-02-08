1be45ace612cd408c187c07e33d5c57f44aea01e
Gavin Stark <gavin@gstark.com>
2021-02-08 11:43:33 -0500

    Arrays and looping arrays

--- java-script-variables/public/main.js
+++ java-script-variables/public/main.js
@@ -48,6 +48,71 @@ function main() {
   const message = `Congratulations ${answer} is correct. You have ${score} points`
 
   console.log(message)
+
+  // Makes an object, car is a variable of type "object"
+  // it has no properties
+  const car = {}
+
+  // car has no properties so...
+  console.log(car.make)
+
+  // person is a variable of type 'object'
+  // it has *four* properties
+  const person = {
+    // the name property, the value is 'Gavin
+    name: 'Gavin',
+
+    // the favoriteColor property has the value 'blue'
+    favoriteColor: 'blue',
+    department: 'teaching',
+
+    // Properties can have different types
+    salary: 1000000,
+
+    hireDate: 2016,
+    'firing date': 2024,
+
+    'special-feature': 'Talks slow',
+  }
+
+  console.log(person.name)
+  console.log(person.salary)
+  console.log(person)
+
+  car.make = 'Ford'
+  car.model = 'Mustang'
+  car.year = 1969
+
+  // Just like above, but using
+  // BRACKET syntax instead of DOT syntax
+  car['make'] = 'Ford'
+  car['model'] = 'Mustang'
+  car['year'] = 1969
+  car['special-feature'] = 'Go fast'
+
+  console.log(car.make)
+  console.log(car)
+
+  // Creating arrays with `new Array` is confusing.
+  // let newArray = new Array(42, 100)
+  // console.log(newArray)
+
+  // let newArrayOne = new Array(12)
+  // console.log(newArrayOne)
+
+  let people = ['Betty', 'Wilma', 'Fred']
+  let collection = ['Betty', 98, 'Fred', 12, 42]
+  console.log(collection)
+
+  let colors = ['red', 'green', 'blue']
+  // C# for(var index = 0; index < colors.Length; index++)
+  for (let index = 0; index < colors.length; index++) {
+    // C#   var currentColor = colors[index]
+    let currentColor = colors[index]
+
+    // C#    Console.WriteLine(currentColor)
+    console.log(currentColor)
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
