573c5ac42bc350d5866e303d7324ce004136fecd
Gavin Stark <gavin@gstark.com>
2021-02-09 11:05:42 -0500

    forEach loops over the elements of the array and calls the function

--- java-script-enumeration/public/main.js
+++ java-script-enumeration/public/main.js
@@ -1,6 +1,41 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  const colors = ['red', 'green', 'blue']
+
+  // Old-school (from yesterday...)
+  //
+  // for(let index = 0; ...)
+
+  function logSomeColor(currentColor) {
+    console.log(`The color is ${currentColor}`)
+  }
+
+  // C#:  foreach(var color in colors)
+
+  //
+  // array
+  //   |
+  //   |   foreach-equivalent
+  //   |     |
+  //   |     |       work to do
+  //   |     |        |
+  //   |     |        V
+  colors.forEach(logSomeColor)
+
+  // length is a property of the array that tells us how long the array is
+  colors.length
+
+  // forEach is a property of the array that is a FUNCTION that loops over the contents of the array
+  colors.forEach
+
+  const person = {
+    // state
+    name: 'Gavin',
+    // state
+    favoriteColor: 'blue',
+    // state
+    salary: 1000000,
+    // behavior
+    loggingFunction: logSomeColor,
   }
 }
 
