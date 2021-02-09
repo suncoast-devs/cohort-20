42bbd9d0ca972893951ba31ea561800f7386b539
Gavin Stark <gavin@gstark.com>
2021-02-09 10:24:32 -0500

    Examples of functions

--- java-script-functions/public/main.js
+++ java-script-functions/public/main.js
@@ -1,7 +1,29 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  // C#
+  //    public int square(int number)
+  function square(number) {
+    return number * number
   }
+
+  // C#
+  //
+  //   public int addOne(int number)
+  function addOne(number) {
+    return number + 1
+  }
+
+  // C#    var myNumber = 42
+  const myNumber = 42
+  // C#   var myAnswer = square(myNumber)
+  const myAnswer = square(myNumber)
+  console.log(myAnswer)
+
+  const myOtherAnswer = addOne(myNumber)
+  console.log(myOtherAnswer)
+
+  const myName = 'Gavin'
+  const myAnswerWithName = addOne(myName)
+  console.log(myAnswerWithName)
 }
 
 document.addEventListener('DOMContentLoaded', main)
