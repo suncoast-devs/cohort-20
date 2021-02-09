65d8d7b5bb18690e1b237341801223ae659c1d2e
Gavin Stark <gavin@gstark.com>
2021-02-09 10:40:16 -0500

    Example of passing functions as variables/arguments

--- java-script-functions/public/main.js
+++ java-script-functions/public/main.js
@@ -1,7 +1,13 @@
 function main() {
   // C#
   //    public int square(int number)
-  function square(number) {
+  // function square(number) {
+  //   return number * number
+  // }
+
+  // That above is just friendly syntax
+  // for the following
+  const square = function (number) {
     return number * number
   }
 
@@ -24,6 +30,22 @@ function main() {
   const myName = 'Gavin'
   const myAnswerWithName = addOne(myName)
   console.log(myAnswerWithName)
+
+  function printIt(array, func) {
+    for (let index = 0; index < array.length; index++) {
+      const value = array[index]
+
+      // const result = square(value)
+      // const result = addOne(value)
+      const result = func(value)
+
+      console.log(`Function turned ${value} into ${result}`)
+    }
+  }
+
+  const numbers = [1, 2, 3, 4, 5]
+  printIt(numbers, square)
+  printIt(numbers, addOne)
 }
 
 document.addEventListener('DOMContentLoaded', main)
