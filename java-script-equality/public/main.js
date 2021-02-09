7536c1ffc4e501b50b980bfd9438ed46f62dc99f
Gavin Stark <gavin@gstark.com>
2021-02-09 10:08:36 -0500

    equals examples

--- java-script-equality/public/main.js
+++ java-script-equality/public/main.js
@@ -1,6 +1,16 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  const number1 = '100'
+  const number2 = 100
+
+  //    number1 != number2
+  if (number1 !== number2) {
+    console.log('The two things are not strictly equal')
+  }
+
+  if (number1 === number2) {
+    console.log('These are the same!')
+  } else {
+    console.log('These are different!')
   }
 }
 
