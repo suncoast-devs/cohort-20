3ad68be8daf95743b5d52a26d0d3f4092cc91746
Gavin Stark <gavin@gstark.com>
2021-02-10 11:11:36 -0500

    Can toggle between players

--- javascript-document-object-model/public/main.js
+++ javascript-document-object-model/public/main.js
@@ -1,10 +1,22 @@
+// @ts-check
 function main() {
+  let currentPlayer = 'X'
+
   const allSquares = document.querySelectorAll('li')
 
   function handleClickSquare(eventThatHappened) {
     const thingClickedOn = eventThatHappened.target
 
-    thingClickedOn.textContent = 'X'
+    thingClickedOn.textContent = currentPlayer
+
+    // If the currentPlayer is exactly the text 'X'
+    if (currentPlayer === 'X') {
+      // Make the current player 'O'
+      currentPlayer = 'O'
+    } else {
+      // The currentPlayer must have been 'O', so become 'X'
+      currentPlayer = 'X'
+    }
   }
 
   allSquares.forEach(listItem =>
