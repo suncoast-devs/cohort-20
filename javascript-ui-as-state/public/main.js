39f346a962c60119cdb2365f429c6e8ae8d58ee0
Gavin Stark <gavin@gstark.com>
2021-02-16 10:50:16 -0500

    Dynamically generate the content of our page
    Makes the state/event/ui loop better

--- javascript-ui-as-state/public/main.js
+++ javascript-ui-as-state/public/main.js
@@ -1,12 +1,35 @@
-let counter = 0
+let counter = 99
 
-function main() {
+function render() {
+  const html = `
+  <p>${counter}</p>
+  <button>Increment</button>
+`
+
+  document.body.innerHTML = html
+
   document.querySelector('button').addEventListener('click', function (event) {
     counter++
 
-    const counterElement = document.querySelector('p')
-    counterElement.textContent = counter
+    // This is no longer needed...
+    // const counterElement = document.querySelector('p')
+    // counterElement.textContent = counter
+
+    // What I need is to re-render the page
+    render()
   })
+}
+
+function main() {
+  render()
+
+  // This isn't needed
+  // document.querySelector('button').addEventListener('click', function (event) {
+  //   counter++
+
+  //   const counterElement = document.querySelector('p')
+  //   counterElement.textContent = counter
+  // })
 }
 
 document.addEventListener('DOMContentLoaded', main)
