b470669a744d79d313c6c973e3ff240bd6c5e8b4
Gavin Stark <gavin@gstark.com>
2021-02-16 11:04:55 -0500

    Event handlers connected to render

--- javascript-ui-as-state/public/main.js
+++ javascript-ui-as-state/public/main.js
@@ -1,3 +1,9 @@
+let teamOneName = 'SDG JavaScripters'
+let teamOneScore = 12
+
+let teamTwoName = 'SDG SeeSharpers'
+let teamTwoScore = 15
+
 function render() {
   const html = `
   <header>
@@ -5,10 +11,10 @@ function render() {
   </header>
   <main>
     <section class="team1">
-      <h2>Team 1</h2>
-      <h3>0</h3>
+      <h2>${teamOneName}</h2>
+      <h3>${teamOneScore}</h3>
       <fieldset>
-        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Name" value="${teamOneName}" />
       </fieldset>
 
       <fieldset>
@@ -18,10 +24,10 @@ function render() {
     </section>
 
     <section class="team2">
-      <h2>Team 2</h2>
-      <h3>0</h3>
+      <h2>${teamTwoName}</h2>
+      <h3>${teamTwoScore}</h3>
       <fieldset>
-        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Name" value="${teamTwoName}" />
       </fieldset>
 
       <fieldset>
@@ -33,6 +39,50 @@ function render() {
 `
 
   document.body.innerHTML = html
+
+  document
+    .querySelector('.team1 .add')
+    .addEventListener('click', function (event) {
+      teamOneScore++
+
+      render()
+    })
+  document
+    .querySelector('.team1 .subtract')
+    .addEventListener('click', function (event) {
+      teamOneScore--
+
+      render()
+    })
+  document
+    .querySelector('.team1 input')
+    .addEventListener('input', function (event) {
+      teamOneName = event.target.value
+
+      render()
+    })
+
+  document
+    .querySelector('.team2 .add')
+    .addEventListener('click', function (event) {
+      teamTwoScore++
+
+      render()
+    })
+  document
+    .querySelector('.team2 .subtract')
+    .addEventListener('click', function (event) {
+      teamTwoScore--
+
+      render()
+    })
+  document
+    .querySelector('.team2 input')
+    .addEventListener('input', function (event) {
+      teamTwoName = event.target.value
+
+      render()
+    })
 }
 
 function main() {
