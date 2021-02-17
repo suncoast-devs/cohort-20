82700f3fac57a40198471cde21ceff0f060878a0
Gavin Stark <gavin@gstark.com>
2021-02-17 11:54:26 -0500

    Now our NewsArticle is customizable and reusable

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 
+// Tell javascript to go get that class (component)
+// from the file we just created.
+import { NewsArticle } from './components/NewsArticle'
+
 export class App extends Component {
   render() {
     return (
       <div className="all-main-content">
         <main>
-          <article className="intro-article">
+          <NewsArticle
+            title="SDG Announces Hackathon!"
+            body="SDG announces the 2020 Summer Hackathon. Join us for an exciting weekend"
+          />
+          <NewsArticle
+            title="Student Graduation is Right Around the Corner"
+            body="Our next cohort of students will be graduating in just over a week."
+          />
+          <NewsArticle
+            title="SDG Standardizes on React"
+            body="React is the best library for learning front end Web"
+          />
+          {/* <article className="intro-article">
             <h2 className="article-title">SDG Announces Hackathon!</h2>
             <p>
               SDG announces the 2020 Summer Hackathon. Join us for an exciting
@@ -27,7 +43,7 @@ export class App extends Component {
             <a className="read-more" href="#here">
               read more about Student Graduation is Right Around the Corner
             </a>
-          </article>
+          </article> */}
         </main>
       </div>
     )
--- hello-react/src/components/NewsArticle.jsx
+++ hello-react/src/components/NewsArticle.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+// C#:     $"Hello {name}"
+// JS:     `Hello ${name}"
+// JSX:     {name}
+
 // export the class so we can use it elsewhere
 //
 //     keyword form javascript declaring a class
@@ -13,6 +17,16 @@ export class NewsArticle extends React.Component {
   // Our one and only absolute requirement is a `render` method
   // that `return`s some JSX
   render() {
-    return <div>Something</div>
+    console.log(this.props)
+
+    return (
+      <article className="intro-article">
+        <h2 className="article-title">{this.props.title}</h2>
+        <p>{this.props.body}</p>
+        <a className="read-more" href="#here">
+          read more about SDG Announces Hackathon!
+        </a>
+      </article>
+    )
   }
 }
