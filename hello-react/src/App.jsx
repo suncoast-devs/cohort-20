815a6cc4b11057eec1b7b1fadc633c221c94d10d
Gavin Stark <gavin@gstark.com>
2021-02-17 11:21:04 -0500

    Some comments and adding some elements to our first simple component

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <p>
+        Hello, World! <a href="sunocast.io">Hello</a>{' '}
+        <span>Hello, here is more text that I want </span> React is neat!!
+      </p>
+    )
   }
 }
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,6 +1,21 @@
+// This pulls in the react code we need
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+// This pulls in our css file!
 import './index.scss'
+
+// This imports a class called `App` from
+// a file named `App.jsx` in the current directory
 import { App } from './App'
 
+// Bootstraps react.
+//
+// It finds a component named App
+//
+// and puts it at the spot in the html
+//
+// where an element with the id of root is
+//
+// In our case, it becomes the entire page
 ReactDOM.render(<App />, document.getElementById('root'))
