0c0361f5c5675ebacc95c72a1ae6f56cbe23d166
Gavin Stark <gavin@gstark.com>
2021-02-18 11:20:42 -0500

    Filling in the board from state using a 2-dimensional array

--- react-tic-tac-toe/src/App.jsx
+++ react-tic-tac-toe/src/App.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    data: [
+      ['_', 'x', '_'],
+      ['_', '_', '_'],
+      ['x', '_', 'o'],
+    ],
+  }
+
   render() {
     return (
       <div>
         <h1>Tic Tac Toe</h1>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>{this.state.data[0][0]}</li>
+          <li>{this.state.data[0][1]}</li>
+          <li>{this.state.data[0][2]}</li>
+
+          <li>{this.state.data[1][0]}</li>
+          <li>{this.state.data[1][1]}</li>
+          <li>{this.state.data[1][2]}</li>
+
+          <li>{this.state.data[2][0]}</li>
+          <li>{this.state.data[2][1]}</li>
+          <li>{this.state.data[2][2]}</li>
         </ul>
       </div>
     )
