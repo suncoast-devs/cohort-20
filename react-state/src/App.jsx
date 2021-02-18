40a3b1f87cf1b52d08b129ea8da67b7294794308
Gavin Stark <gavin@gstark.com>
2021-02-18 10:20:10 -0500

    Extracted a component in the same file

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 
+export class Counter extends Component {
+  render() {
+    return (
+      <section>
+        <p>Current Value: 0</p>
+        <button>Increment</button>
+      </section>
+    )
+  }
+}
+
 export class App extends Component {
   render() {
     return (
       <div>
         <header>Welcome to my Counter</header>
 
-        <section>
-          <p>Current Value: 0</p>
-          <button>Increment</button>
-        </section>
+        <Counter />
 
         <footer>Thanks for using my app!</footer>
       </div>
