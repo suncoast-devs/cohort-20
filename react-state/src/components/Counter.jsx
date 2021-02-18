e54e1ec714644f12c24d004143c7c6f6a67a6893
Gavin Stark <gavin@gstark.com>
2021-02-18 10:36:23 -0500

    Hook up a handler to a button

--- react-state/src/components/Counter.jsx
+++ react-state/src/components/Counter.jsx
@@ -6,11 +6,15 @@ export class Counter extends Component {
     counter: 0,
   }
 
+  handleClickButton() {
+    console.log('We did it!')
+  }
+
   render() {
     return (
       <section>
         <p>Current Value: {this.state.counter}</p>
-        <button>Increment</button>
+        <button onClick={this.handleClickButton}>Increment</button>
       </section>
     )
   }
