8201e27e3470a034fb4d91082afc54333fa2ad38
Gavin Stark <gavin@gstark.com>
2021-02-18 10:31:19 -0500

    Introduces a state for "counter"

--- react-state/src/components/Counter.jsx
+++ react-state/src/components/Counter.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 export class Counter extends Component {
+  // The current state of the counter component
+  state = {
+    counter: 0,
+  }
+
   render() {
     return (
       <section>
-        <p>Current Value: 0</p>
+        <p>Current Value: {this.state.counter}</p>
         <button>Increment</button>
       </section>
     )
