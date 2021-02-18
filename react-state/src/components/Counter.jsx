07eabe7c2f74271f86c05f215cf6fd87be07f6c0
Gavin Stark <gavin@gstark.com>
2021-02-18 10:49:25 -0500

    Uses React's special setState to change the state and have react redraw the component

--- react-state/src/components/Counter.jsx
+++ react-state/src/components/Counter.jsx
@@ -3,11 +3,30 @@ import React, { Component } from 'react'
 export class Counter extends Component {
   // The current state of the counter component
   state = {
-    counter: 0,
+    counter: 42,
   }
 
-  handleClickButton() {
-    console.log('We did it!')
+  handleClickButton = () => {
+    // What do we really want to do?
+    //
+    // Algorithm?
+    //
+    // Get the current value of the counter from the state
+
+    let currentCounterValue = this.state.counter
+
+    // Increment it by 1
+    currentCounterValue = currentCounterValue + 1
+
+    // Update the state
+    // Never, ever, never ever, directly change this.state
+    // this.state.counter = currentCounterValue
+
+    // React gives us a *special* way to update state so that it knows
+    const newState = {
+      counter: currentCounterValue,
+    }
+    this.setState(newState)
   }
 
   render() {
