049e25e98e12d5a17d6b85a26bcb218fca12db2d
Gavin Stark <gavin@gstark.com>
2021-02-18 10:24:33 -0500

    Extract the component using Visual Studio "move to new file"

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -1,15 +1,5 @@
 import React, { Component } from 'react'
-
-export class Counter extends Component {
-  render() {
-    return (
-      <section>
-        <p>Current Value: 0</p>
-        <button>Increment</button>
-      </section>
-    )
-  }
-}
+import { Counter } from './Counter'
 
 export class App extends Component {
   render() {
---
+++ react-state/src/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Counter extends Component {
+  render() {
+    return (
+      <section>
+        <p>Current Value: 0</p>
+        <button>Increment</button>
+      </section>
+    )
+  }
+}
