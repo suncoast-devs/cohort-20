0e9fd8fb1f59f2fd9aa0d88aab37017e49acba07
Gavin Stark <gavin@gstark.com>
2021-02-19 10:13:18 -0500

    Step 5 - Do what we need to update the state, in this case make a new game by calling the api and shoving the result in state

--- react-tic-tac-toe-revisited/src/App.jsx
+++ react-tic-tac-toe-revisited/src/App.jsx
@@ -14,10 +14,26 @@ export class App extends Component {
     console.log(`I clicked on row ${row} and column ${column}`)
   }
 
+  handleNewGame = async () => {
+    const response = await fetch(
+      'https://sdg-tic-tac-toe-api.herokuapp.com/game',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+
+    const game = await response.json()
+
+    this.setState(game)
+  }
+
   render() {
     return (
       <div>
-        <h1>Tic Tac Toe</h1>
+        <h1>
+          Tic Tac Toe - <button onClick={this.handleNewGame}>NEW GAME</button>
+        </h1>
         <ul>
           <li onClick={() => this.handleClickCell(0, 0)}>
             {this.state.board[0][0]}
