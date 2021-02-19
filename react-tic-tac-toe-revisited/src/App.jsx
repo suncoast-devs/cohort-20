6570d3ba447be5e89873d3b819dc50708a46b4ae
Gavin Stark <gavin@gstark.com>
2021-02-19 09:55:31 -0500

    Step 2 and 3 - Updating our static UI to pull from state (not connected to the API yet, but we can manually change it and see the results)

--- react-tic-tac-toe-revisited/src/App.jsx
+++ react-tic-tac-toe-revisited/src/App.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    board: [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+    ],
+    winner: null,
+  }
+
   render() {
     return (
       <div>
         <h1>Tic Tac Toe</h1>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>{this.state.board[0][0]}</li>
+          <li>{this.state.board[0][1]}</li>
+          <li>{this.state.board[0][2]}</li>
+
+          <li>{this.state.board[1][0]}</li>
+          <li>{this.state.board[1][1]}</li>
+          <li>{this.state.board[1][2]}</li>
+
+          <li>{this.state.board[2][0]}</li>
+          <li>{this.state.board[2][1]}</li>
+          <li>{this.state.board[2][2]}</li>
         </ul>
       </div>
     )
