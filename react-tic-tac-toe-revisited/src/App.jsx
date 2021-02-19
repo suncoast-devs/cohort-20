14993112367daaa86dcd10c5c85aa62fe23e2d1c
Gavin Stark <gavin@gstark.com>
2021-02-19 09:43:48 -0500

    Step 1 - Static HTML and CSS

--- react-tic-tac-toe-revisited/src/App.jsx
+++ react-tic-tac-toe-revisited/src/App.jsx
@@ -2,6 +2,21 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <div>
+        <h1>Tic Tac Toe</h1>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    )
   }
 }
