ddde26a492bc8b5598abaa580364032ff5c39106
Gavin Stark <gavin@gstark.com>
2021-02-19 10:03:40 -0500

    Step 4 - Connecting our events to handling functions (but not yet updating state)

--- react-tic-tac-toe-revisited/src/App.jsx
+++ react-tic-tac-toe-revisited/src/App.jsx
@@ -10,22 +10,44 @@ export class App extends Component {
     winner: null,
   }
 
+  handleClickCell = (row, column) => {
+    console.log(`I clicked on row ${row} and column ${column}`)
+  }
+
   render() {
     return (
       <div>
         <h1>Tic Tac Toe</h1>
         <ul>
-          <li>{this.state.board[0][0]}</li>
-          <li>{this.state.board[0][1]}</li>
-          <li>{this.state.board[0][2]}</li>
+          <li onClick={() => this.handleClickCell(0, 0)}>
+            {this.state.board[0][0]}
+          </li>
+          <li onClick={() => this.handleClickCell(0, 1)}>
+            {this.state.board[0][1]}
+          </li>
+          <li onClick={() => this.handleClickCell(0, 2)}>
+            {this.state.board[0][2]}
+          </li>
 
-          <li>{this.state.board[1][0]}</li>
-          <li>{this.state.board[1][1]}</li>
-          <li>{this.state.board[1][2]}</li>
+          <li onClick={() => this.handleClickCell(1, 0)}>
+            {this.state.board[1][0]}
+          </li>
+          <li onClick={() => this.handleClickCell(1, 1)}>
+            {this.state.board[1][1]}
+          </li>
+          <li onClick={() => this.handleClickCell(1, 2)}>
+            {this.state.board[1][2]}
+          </li>
 
-          <li>{this.state.board[2][0]}</li>
-          <li>{this.state.board[2][1]}</li>
-          <li>{this.state.board[2][2]}</li>
+          <li onClick={() => this.handleClickCell(2, 0)}>
+            {this.state.board[2][0]}
+          </li>
+          <li onClick={() => this.handleClickCell(2, 1)}>
+            {this.state.board[2][1]}
+          </li>
+          <li onClick={() => this.handleClickCell(2, 2)}>
+            {this.state.board[2][2]}
+          </li>
         </ul>
       </div>
     )
