ac9f96795210958314456a00e32569191c705494
Gavin Stark <gavin@gstark.com>
2021-02-22 09:50:25 -0500

    Static board but from STATE instead of hard coding (at least the start)

--- mine-sweeper/src/App.jsx
+++ mine-sweeper/src/App.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    board: [
+      ['*', '1', '4', ' ', ' ', ' ', ' ', ' '],
+      [' ', 'F', ' ', ' ', '2', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+  }
+
   render() {
     return (
       <main>
         <h1>Mine Sweeper</h1>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>{this.state.board[0][0]}</li>
+          <li>{this.state.board[0][1]}</li>
+          <li>{this.state.board[0][2]}</li>
+          <li>{this.state.board[0][3]}</li>
           <li></li>
           <li></li>
           <li></li>
