f79de8528aa24593d1e8e7bb963150716ed632f3
Gavin Stark <gavin@gstark.com>
2021-02-24 12:14:48 -0500

    Extract and move our component to its own file in the components directory

--- one-list-react-client/src/App.jsx
+++ one-list-react-client/src/App.jsx
@@ -1,37 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import sdgLogo from './images/sdg-logo.png'
-
-// This kinda looks like arguments to a function
-// function(name) { console.log(name) }
-//
-// Except they are wrapped in a `{    }` -- that means destructuring
-//
-// Since props is the first argument, we are automatically destructuring props
-export function TodoItemComponent({ id, text, complete, reloadItems }) {
-  async function toggleCompleteStatus() {
-    console.log(`I clicked on an item with id ${id}!`)
-    // Call the API HERE to tell it that an item is complete (or incomplete)
-
-    const response = await axios.put(
-      `https://one-list-api.herokuapp.com/items/${id}?access_token=cohort20`,
-      { item: { complete: !complete } }
-    )
-
-    console.log(response.data)
-
-    // reloadItems is a prop
-    // -- but it is a function the parent is giving me
-    // -- so I can call it here, the appropriate time to reload the items
-    reloadItems()
-  }
-
-  return (
-    <li onClick={toggleCompleteStatus} className={complete ? 'completed' : ''}>
-      {text}
-    </li>
-  )
-}
+import { TodoItemComponent } from './components/TodoItemComponent'
 
 export function App() {
   const [todoItems, setTodoItems] = useState([])
