1440fa0dbbefc7858f89dfaa03b5ac97d3c7fc8b
Gavin Stark <gavin@gstark.com>
2021-02-24 11:16:37 -0500

    Adds a second useEffect as a demo

--- one-list-react-client/src/App.jsx
+++ one-list-react-client/src/App.jsx
@@ -10,18 +10,27 @@ export function App() {
   // - The function is also called when the array's contents change
   //
   // But alas, our array is empty so we are called only once
-  useEffect(async function () {
-    console.log('Run once when the app mounts')
 
-    // Right here is where we want our API fetching code to go
-    const response = await axios.get(
-      'https://one-list-api.herokuapp.com/items?access_token=cohort20'
-    )
+  useEffect(
+    async function () {
+      console.log('Run once when the app mounts')
 
-    // Don't even need to do   const json = await response.json()
-    // axios does it for us automatically, we just ask for `data`
+      // Right here is where we want our API fetching code to go
+      const response = await axios.get(
+        'https://one-list-api.herokuapp.com/items?access_token=illustriousvoyage'
+      )
 
-    setTodoItems(response.data)
+      // Don't even need to do   const json = await response.json()
+      // axios does it for us automatically, we just ask for `data`
+
+      setTodoItems(response.data)
+    },
+    // Because the second argument here is an empty array, there is nothing to watch
+    []
+  )
+
+  useEffect(function () {
+    console.log('this too is called only once when the app loads')
   }, [])
 
   // No API fetching here, otherwise it would happen on ever render
