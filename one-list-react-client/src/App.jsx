cba972136fa22945c46b8f8360333ea63de545cd
Gavin Stark <gavin@gstark.com>
2021-02-25 11:16:19 -0500

    Refactor our TodoItemPage into the pages folder

--- one-list-react-client/src/App.jsx
+++ one-list-react-client/src/App.jsx
@@ -1,57 +1,8 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Route, Switch, useHistory, useParams } from 'react-router'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { Route, Switch } from 'react-router'
 import sdgLogo from './images/sdg-logo.png'
 import { TodoListPage } from './pages/TodoListPage'
-
-export function TodoItemPage() {
-  const [todoItem, setTodoItem] = useState({
-    id: undefined,
-    text: '',
-    complete: false,
-  })
-
-  // This gives us the ability to navigate the browser
-  const history = useHistory()
-
-  // this gives us back an *OBJECT* with neat properties
-  const params = useParams()
-
-  useEffect(
-    async function () {
-      // load the specific item from the API
-
-      const response = await axios.get(
-        `https://one-list-api.herokuapp.com/items/${params.id}?access_token=cohort20`
-      )
-
-      setTodoItem(response.data)
-    },
-    [params.id]
-  )
-
-  async function deleteTodoItem() {
-    const response = await axios.delete(
-      `https://one-list-api.herokuapp.com/items/${params.id}?access_token=cohort20`
-    )
-
-    // But now we want to redirect to the home page
-    history.push('/')
-  }
-
-  return (
-    <div>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p className={todoItem.complete ? 'completed' : ''}>{todoItem.text}</p>
-      <p>{todoItem.created_at}</p>
-      <p>{todoItem.updated_at}</p>
-      <button onClick={deleteTodoItem}>Delete</button>
-    </div>
-  )
-}
+import { TodoItemPage } from './pages/TodoItemPage'
 
 export function App() {
   return (
---
+++ one-list-react-client/src/pages/TodoItemPage.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useHistory, useParams } from 'react-router'
+import { Link } from 'react-router-dom'
+
+export function TodoItemPage() {
+  const [todoItem, setTodoItem] = useState({
+    id: undefined,
+    text: '',
+    complete: false,
+  })
+
+  // This gives us the ability to navigate the browser
+  const history = useHistory()
+
+  // this gives us back an *OBJECT* with neat properties
+  const params = useParams()
+
+  useEffect(
+    async function () {
+      // load the specific item from the API
+      const response = await axios.get(
+        `https://one-list-api.herokuapp.com/items/${params.id}?access_token=cohort20`
+      )
+
+      setTodoItem(response.data)
+    },
+    [params.id]
+  )
+
+  async function deleteTodoItem() {
+    const response = await axios.delete(
+      `https://one-list-api.herokuapp.com/items/${params.id}?access_token=cohort20`
+    )
+
+    // But now we want to redirect to the home page
+    history.push('/')
+  }
+
+  return (
+    <div>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+      <p className={todoItem.complete ? 'completed' : ''}>{todoItem.text}</p>
+      <p>{todoItem.created_at}</p>
+      <p>{todoItem.updated_at}</p>
+      <button onClick={deleteTodoItem}>Delete</button>
+    </div>
+  )
+}
