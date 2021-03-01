de440fd936def0ae3ccb356be1901168ae2e9e4b
Gavin Stark <gavin@gstark.com>
2021-03-01 11:05:41 -0500

    Adds the database context relationship between "Companies" (database table name) and our model "Company"

--- JobTracker/Models/DatabaseContext.cs
+++ JobTracker/Models/DatabaseContext.cs
@@ -14,6 +14,8 @@ namespace JobTracker.Models
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
         // Add database tables here
+        public DbSet<Company> Companies { get; set; }
+
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
