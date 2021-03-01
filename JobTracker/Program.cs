e87df4c1168dad0a4600aa7e06279a45661d2de4
Gavin Stark <gavin@gstark.com>
2021-03-01 09:47:23 -0500

    Initial Commit

---
+++ JobTracker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JobTracker.Models;
+using System.Linq;
+
+namespace JobTracker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
