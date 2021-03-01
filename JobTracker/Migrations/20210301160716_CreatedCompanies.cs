0a36841fcccdbb1c658bf6ae3dbeccd247642abf
Gavin Stark <gavin@gstark.com>
2021-03-01 11:13:12 -0500

    Added a migration to generate a table for our Companies

---
+++ JobTracker/Migrations/20210301160716_CreatedCompanies.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace JobTracker.Migrations
+{
+    public partial class CreatedCompanies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Position = table.Column<string>(type: "text", nullable: true),
+                    Technology = table.Column<string>(type: "text", nullable: true),
+                    ContactName = table.Column<string>(type: "text", nullable: true),
+                    ContactEmail = table.Column<string>(type: "text", nullable: true),
+                    Remote = table.Column<bool>(type: "boolean", nullable: false),
+                    ContactCount = table.Column<int>(type: "integer", nullable: false),
+                    CompanyName = table.Column<string>(type: "text", nullable: true),
+                    Salary = table.Column<int>(type: "integer", nullable: false),
+                    Benefits = table.Column<bool>(type: "boolean", nullable: false),
+                    CompanyLogo = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
