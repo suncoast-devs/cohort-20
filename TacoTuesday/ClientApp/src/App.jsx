da54d991555ef5f28984280e67dd458333aefd5d
Gavin Stark <gavin@gstark.com>
2021-03-08 11:10:00 -0500

    Static HTML (JSX) and CSS for pages that came from my wireframes

--- TacoTuesday/ClientApp/src/App.jsx
+++ TacoTuesday/ClientApp/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
+import { Restaurants } from './pages/Restaurants'
+
 import './custom.scss'
 
 export function App() {
-  return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
-  )
+  return <Restaurants />
 }
