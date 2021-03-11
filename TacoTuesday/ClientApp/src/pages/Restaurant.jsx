9b9f905da6cb440ebb6ec38c1a48bbf0fa8e4885
Gavin Stark <gavin@gstark.com>
2021-03-11 11:11:19 -0500

    Adds a controller to make an API for creating restaurants - also adds state, and handlers for the user interface

--- TacoTuesday/ClientApp/src/pages/Restaurant.jsx
+++ TacoTuesday/ClientApp/src/pages/Restaurant.jsx
@@ -1,9 +1,11 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
+import { useHistory, useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 import avatar from '../images/avatar.png'
 
 export function Restaurant() {
+  const history = useHistory()
   const params = useParams()
   const [restaurant, setRestaurant] = useState({
     name: '',
@@ -14,7 +16,14 @@ export function Restaurant() {
   })
 
   // This gives us the ID of the restaurant we are looking for
-  const id = params.id
+  const id = Number(params.id)
+
+  const [newReview, setNewReview] = useState({
+    body: '',
+    summary: '',
+    stars: 0,
+    restaurantId: id,
+  })
 
   useEffect(() => {
     async function fetchRestaurant() {
@@ -30,6 +39,50 @@ export function Restaurant() {
 
     fetchRestaurant()
   }, [id])
+
+  function handleNewReviewTextFieldChange(event) {
+    const name = event.target.name
+    const value = event.target.value
+
+    setNewReview({ ...newReview, [name]: value })
+  }
+
+  function handleStarRadioButton(newStars) {
+    setNewReview({ ...newReview, stars: newStars })
+  }
+
+  async function handleNewReviewSubmit(event) {
+    event.preventDefault()
+
+    // const response = await axios({
+    //   method: 'POST',
+    //   url: 'api/reviews',
+    //   data: newReview,
+    // })
+
+    const response = await fetch(`/api/Reviews`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newReview),
+    })
+
+    // setTimeout(function () {
+    //   // Do this after a break
+    //   history.push('/')
+    // }, 5000)
+
+    setNewReview({
+      ...newReview,
+      body: '',
+      summary: '',
+      stars: 0,
+    })
+
+    const restaurantsResponse = await fetch(`/api/Restaurants/${id}`)
+    const apiData = await restaurantsResponse.json()
+
+    setRestaurant(apiData)
+  }
 
   return (
     <>
@@ -89,10 +142,16 @@ export function Restaurant() {
           })}
         </ul>
         <h3>Enter your own review</h3>
-        <form action="#">
+
+        <form action="#" onSubmit={handleNewReviewSubmit}>
           <p className="form-input">
             <label htmlFor="summary">Summary</label>
-            <input type="text" name="summary" />
+            <input
+              type="text"
+              name="summary"
+              value={newReview.summary}
+              onChange={handleNewReviewTextFieldChange}
+            />
             <span className="note">
               Enter a brief summary of your review. Example:{' '}
               <strong>Great food, good prices.</strong>
@@ -100,18 +159,57 @@ export function Restaurant() {
           </p>
           <p className="form-input">
             <label htmlFor="body">Review</label>
-            <textarea name="body"></textarea>
+            <textarea
+              name="body"
+              value={newReview.body}
+              onChange={handleNewReviewTextFieldChange}
+            ></textarea>
           </p>
           <div className="rating">
-            <input id="star-rating-1" type="radio" name="stars" value="1" />
+            <input
+              id="star-rating-1"
+              type="radio"
+              name="stars"
+              value="1"
+              checked={newReview.stars === 1}
+              onChange={() => handleStarRadioButton(1)}
+            />
             <label htmlFor="star-rating-1">1 star</label>
-            <input id="star-rating-2" type="radio" name="stars" value="2" />
+            <input
+              id="star-rating-2"
+              type="radio"
+              name="stars"
+              value="2"
+              checked={newReview.stars === 2}
+              onChange={() => handleStarRadioButton(2)}
+            />
             <label htmlFor="star-rating-2">2 stars</label>
-            <input id="star-rating-3" type="radio" name="stars" value="3" />
+            <input
+              id="star-rating-3"
+              type="radio"
+              name="stars"
+              value="3"
+              checked={newReview.stars === 3}
+              onChange={() => handleStarRadioButton(3)}
+            />
             <label htmlFor="star-rating-3">3 stars</label>
-            <input id="star-rating-4" type="radio" name="stars" value="4" />
+            <input
+              id="star-rating-4"
+              type="radio"
+              name="stars"
+              value="4"
+              checked={newReview.stars === 4}
+              onChange={() => handleStarRadioButton(4)}
+            />
             <label htmlFor="star-rating-4">4 stars</label>
-            <input id="star-rating-5" type="radio" name="stars" value="5" />
+            <input
+              id="star-rating-5"
+              type="radio"
+              name="stars"
+              value="5"
+              checked={newReview.stars === 5}
+              onChange={() => handleStarRadioButton(5)}
+            />
             <label htmlFor="star-rating-5">5 stars</label>
 
             <div className="star-rating">
