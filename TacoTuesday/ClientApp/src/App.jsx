69432dc3bf477dec5ee9b6674ba1216ecd5ffef4
Gavin Stark <gavin@gstark.com>
2021-03-15 10:06:06 -0400

    Adds a layout component as a demo

--- TacoTuesday/ClientApp/src/App.jsx
+++ TacoTuesday/ClientApp/src/App.jsx
@@ -2,46 +2,32 @@ import React from 'react'
 import { NewRestaurant } from './pages/NewRestaurant'
 
 import './custom.scss'
-import avatar from './images/avatar.png'
 
-import { Link, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { Restaurants } from './pages/Restaurants'
 import { Restaurant } from './pages/Restaurant'
+import { WithHeaderAndFooter } from './components/WithHeaderAndFooter'
 
 export function App() {
   return (
     <>
-      <header>
-        <ul>
-          <li>
-            <nav>
-              <Link to="/new">
-                <i className="fa fa-plus"></i> Restaurant
-              </Link>
-              <p>Welcome back, Steve!</p>
-            </nav>
-          </li>
-          <li className="avatar">
-            <img src={avatar} alt="Steve's Avatar" height="64" width="64" />
-          </li>
-        </ul>
-      </header>
       <Switch>
         <Route exact path="/">
-          <Restaurants />
+          <WithHeaderAndFooter>
+            <Restaurants />
+          </WithHeaderAndFooter>
         </Route>
         <Route exact path="/new">
-          <NewRestaurant />
+          <WithHeaderAndFooter>
+            <NewRestaurant />
+          </WithHeaderAndFooter>
         </Route>
         <Route exact path="/restaurants/:id">
-          <Restaurant />
+          <WithHeaderAndFooter>
+            <Restaurant />
+          </WithHeaderAndFooter>
         </Route>
       </Switch>
-      <footer>
-        <p>
-          Built with <i className="fa fa-heart"></i> in St Petersburg, Florida.
-        </p>
-      </footer>
     </>
   )
 }
---
+++ TacoTuesday/ClientApp/src/components/WithHeaderAndFooter.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import avatar from '../images/avatar.png'
+import { Link } from 'react-router-dom'
+
+export function WithHeaderAndFooter(props) {
+  return (
+    <>
+      <header>
+        <ul>
+          <li>
+            <nav>
+              <Link to="/new">
+                <i className="fa fa-plus"></i> Restaurant
+              </Link>
+              <p>Welcome back, Steve!</p>
+            </nav>
+          </li>
+          <li className="avatar">
+            <img src={avatar} alt="Steve's Avatar" height="64" width="64" />
+          </li>
+        </ul>
+      </header>
+      {props.children}
+      <footer>
+        <p>
+          Built with <i className="fa fa-heart"></i> in St Petersburg, Florida.
+        </p>
+      </footer>
+    </>
+  )
+}
