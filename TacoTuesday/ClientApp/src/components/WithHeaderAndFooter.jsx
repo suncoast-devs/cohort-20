8d0c1c648fb0c6da4da5a8aeb109b1a729682c18
Gavin Stark <gavin@gstark.com>
2021-03-15 11:55:00 -0400

    Updates our header and restraurant page to selectively include or exclude forms and buttons depending on the result of `isLoggedIn()`

--- TacoTuesday/ClientApp/src/components/WithHeaderAndFooter.jsx
+++ TacoTuesday/ClientApp/src/components/WithHeaderAndFooter.jsx
@@ -1,25 +1,48 @@
 import React from 'react'
 import avatar from '../images/avatar.png'
 import { Link } from 'react-router-dom'
+import { getUser, isLoggedIn, logout } from '../auth'
 
 export function WithHeaderAndFooter(props) {
+  function handleLogout() {
+    logout()
+
+    window.location.assign('/')
+  }
+
+  const user = getUser()
+
   return (
     <>
       <header>
         <ul>
           <li>
             <nav>
-              <Link to="/new">
-                <i className="fa fa-plus"></i> Restaurant
-              </Link>
-              <Link to="/signin">Sign In</Link>
-              <Link to="/signup">Sign Up</Link>
-              <p>Welcome back, Steve!</p>
+              {isLoggedIn() && (
+                <Link to="/new">
+                  <i className="fa fa-plus"></i> Restaurant
+                </Link>
+              )}
+              {isLoggedIn() || <Link to="/signin">Sign In</Link>}
+              {isLoggedIn() || <Link to="/signup">Sign Up</Link>}
+              {isLoggedIn() && (
+                <span className="link" onClick={handleLogout}>
+                  Sign out
+                </span>
+              )}
+              {isLoggedIn() && <p>Welcome back, {user.fullName}!</p>}
             </nav>
           </li>
-          <li className="avatar">
-            <img src={avatar} alt="Steve's Avatar" height="64" width="64" />
-          </li>
+          {isLoggedIn() && (
+            <li className="avatar">
+              <img
+                src={avatar}
+                alt={`${user.fullName} Avatar`}
+                height="64"
+                width="64"
+              />
+            </li>
+          )}
         </ul>
       </header>
       {props.children}
