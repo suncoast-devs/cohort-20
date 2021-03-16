2534433e3476faf0fe1fd6fa2395e7928a4eaa6a
Gavin Stark <gavin@gstark.com>
2021-03-16 11:10:39 -0400

    Add Latitude and Longitude to our restaurants model

--- TacoTuesday/Models/Restaurant.cs
+++ TacoTuesday/Models/Restaurant.cs
@@ -14,6 +14,8 @@ namespace TacoTuesday.Models
 
         [Required]
         public string Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
 
         public string Telephone { get; set; }
 
